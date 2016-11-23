e900b30c77e8f6bb63afc4aa922caf1e7eacbe81
Juan Manuel Schillaci <jmschillaci@gmail.com>
2016-11-23 19:32:03 -0300

    Added email search

--- pinaxcon/attendees/admin.py
+++ pinaxcon/attendees/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 from .models import Attendee
 
 
-admin.site.register(Attendee)
+class AttendeeAdmin(admin.ModelAdmin):
+    search_fields = ('user__email', 'full_name')
+    list_display = ('get_email', 'full_name')
+
+    def get_email(self, object):
+        return object.user.email
+
+admin.site.register(Attendee, AttendeeAdmin)
